5de01c1528417f79e70a85cc1b93fb9ebd7dbb3a
Vaibhav DESHPANDE <47456980+vaibhav628@users.noreply.github.com>
2019-04-27 23:36:32 -0400

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'add-ticker', loadChildren: './add-ticker/add-ticker.module#AddTickerPageModule' },
+  { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
